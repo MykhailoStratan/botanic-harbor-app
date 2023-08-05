1ecd48cefa3064e0f1cb583ba15fbe1f47770e1c
Stratan Mykhailo <stratan.eng@gmail.com>
2023-08-05 16:27:45 -0700

    main: initial layout completed for the main page

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,31 @@ function App() {
         <>
             <header className="header-container">
                 <div className="header-user">
-                    <div className="user-logo-container"><img src="" alt="user-logo" className="user-logo" /></div>
+                    <div className="user-logo-container">
+                        <button className="btn-active">
+                            <img src="/user-logo.png" alt="user-logo" className="user-logo" />
+                        </button>
+                    </div>
                     <div className="user-name"><p>Welcome</p>Kosar</div>
                 </div>
                 <div className="header-slogan-container">
-                    <h2 className="header-slogan">Choose your beautiful</h2>
-                    <img src="" alt="" className="header-slogan-img" />
+                    <h2 className="header-slogan">Choose your beautiful
+                        <span className="header-slogan-img-container">
+                            <img src="/plants/plant-1.png" alt="" className="header-slogan-img" />
+                        </span>
+                    </h2> 
                 </div>
                 <div className="header-utilities-container">
-                    <div className="utilities-notifications">
-                        <button className="btn-notifications"></button>
+                    <div className="utilities utilities-notifications">
+                        <button className="btn-active btn-notifications">
+                            <img src="/notification-icon.svg" alt="" className="btn-icon" />
+                        </button>
                         <select name="" id="" className="dropdown-notifications"></select>
                     </div>
-                    <div className="utilities-search">
-                        <button className="btn-search"></button>
+                    <div className="utilities utilities-search">
+                        <button className="btn-active btn-search">
+                            <img src="/search-icon.svg" alt="" className="btn-icon" />
+                        </button>
                         <input name="" id="" className="inpuit-search"></input>
                     </div>
                 </div>
@@ -38,44 +49,50 @@ function App() {
                     <ul>
                         <li className="content-carousel-item">
                             <div className="carousel-item-card">
-                                <input type="text" className="carousel-item-save" /><img src="" alt="" className="carousel-item-image" />
-                                <p className="carousel-item-price"></p>
-                                <div className="carousel-item-rating">
-                                    <img src="" alt="" className="rating-image" />
-                                    <p className="rating-level"></p>
+                                <button className="carousel-item-save"><img src="/heart-filled-icon.svg" alt="" /></button>
+                                <div className="carousel-item-image">
+                                    <img src="/plants/plant-5.png" alt="" />
                                 </div>
+                                <p className="carousel-item-price">$27.5</p>
+                                <p className="carousel-item-rating">
+                                    <img src="/star-icon.svg" alt="" className="rating-image" />3.4
+                                </p>
                             </div>
                             <div className="card-description">
-                                <h3 className="description-plant-name"></h3>
-                                <p className="description-plant-hint"></p>
+                                <h3 className="description-plant-name">Caladium plants</h3>
+                                <p className="description-plant-hint">It needs watering once a week</p>
                             </div>
                         </li>
                         <li className="content-carousel-item">
                             <div className="carousel-item-card">
-                                <input type="text" className="carousel-item-save" /><img src="" alt="" className="carousel-item-image" />
-                                <p className="carousel-item-price"></p>
-                                <div className="carousel-item-rating">
-                                    <img src="" alt="" className="rating-image" />
-                                    <p className="rating-level"></p>
+                                <button className="carousel-item-save"><img src="/heart-filled-icon.svg" alt="" /></button>
+                                <div className="carousel-item-image">
+                                    <img src="/plants/plant-2.jpg" alt="" />
                                 </div>
+                                <p className="carousel-item-price">$27.5</p>
+                                <p className="carousel-item-rating">
+                                    <img src="/star-icon.svg" alt="" className="rating-image" />3.4
+                                </p>
                             </div>
                             <div className="card-description">
-                                <h3 className="description-plant-name"></h3>
-                                <p className="description-plant-hint"></p>
+                                <h3 className="description-plant-name">Caladium plants</h3>
+                                <p className="description-plant-hint">It needs watering once a week</p>
                             </div>
                         </li>
                         <li className="content-carousel-item">
                             <div className="carousel-item-card">
-                                <input type="text" className="carousel-item-save" /><img src="" alt="" className="carousel-item-image" />
-                                <p className="carousel-item-price"></p>
-                                <div className="carousel-item-rating">
-                                    <img src="" alt="" className="rating-image" />
-                                    <p className="rating-level"></p>
+                                <button className="carousel-item-save"><img src="/heart-filled-icon.svg" alt="" /></button>
+                                <div className="carousel-item-image">
+                                    <img src="/plants/plant-6.png" alt="" />
                                 </div>
+                                <p className="carousel-item-price">$27.5</p>
+                                <p className="carousel-item-rating">
+                                    <img src="/star-icon.svg" alt="" className="rating-image" />3.4
+                                </p>
                             </div>
                             <div className="card-description">
-                                <h3 className="description-plant-name"></h3>
-                                <p className="description-plant-hint"></p>
+                                <h3 className="description-plant-name">Caladium plants</h3>
+                                <p className="description-plant-hint">It needs watering once a week</p>
                             </div>
                         </li>
                     </ul>
@@ -85,36 +102,36 @@ function App() {
                 <div className="navigation">
                     <ul>
                         <li className="navigation-item">
-                            <button className="navigation-item-button">
+                            <button className="btn-active navigation-item-button">
                                 <a href="" className="navigation-item-link">
-                                    <img src="" alt="" className="navigation-item-logo" />
-                                    Home
+                                    <img src="/home-icon.svg" alt="Home" className="navigation-item-logo" />
                                 </a>
                             </button>
+                            <p>Home</p>
                         </li>
                         <li className="navigation-item">
-                            <button className="navigation-item-button">
+                            <button className="btn-active navigation-item-button">
                                 <a href="" className="navigation-item-link">
-                                    Favourites
-                                    <img src="" alt="" className="navigation-item-logo" />
+                                    <img src="/heart-big-icon.svg" alt="Favourites" className="navigation-item-logo" />
                                 </a>
                             </button>
+                            <p>Favourites</p>
                         </li>
                         <li className="navigation-item">
-                            <button className="navigation-item-button">
+                            <button className="btn-active navigation-item-button">
                                 <a href="" className="navigation-item-link">
-                                    Cart
-                                    <img src="" alt="" className="navigation-item-logo" />
-                                </a>
+                                    <img src="/cart-icon.svg" alt="" className="navigation-item-logo" />
+                                </a>  
                             </button>
+                            <p>Cart</p>
                         </li>
                         <li className="navigation-item">
-                            <button className="navigation-item-button">
+                            <button className="btn-active navigation-item-button">
                                 <a href="" className="navigation-item-link">
-                                    Profile
-                                    <img src="" alt="" className="navigation-item-logo" />
+                                    <img src="/profile-icon.svg" alt="" className="navigation-item-logo" />
                                 </a>
                             </button>
+                            <p>Profile</p>
                         </li>
                     </ul>
                 </div>
